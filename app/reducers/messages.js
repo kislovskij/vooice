2cb0ae17edface8b0c0cc31d581d8ee6bca82c62
Viktor Kislovskij <viktor.kislovskij@leoburnett.de>
2017-12-18 18:48:50 +0100

    Update Messages Reducer

--- app/reducers/messages.js
+++ app/reducers/messages.js
@@ -9,25 +9,39 @@ type actionType = {
   +type: string
 };
 
-const initialState = {
-  messages: []
-}
-
-export default function messages(state = initialState, action: actionType) {
+const message = (state, action) => {
   switch (action.type) {
     case ADD_MESSAGE:
-      return Object.assign({}, state, {
-        messages: [
-          ...state.messages,
-          {
-            author: action.author,
-            text: action.text
-          }
-        ]
-      })
-    case SEND_MESSAGE:
-      return state - 1;
+      return {
+        id: action.id,
+        text: action.text,
+        time: action.time,
+        author: action.author
+      }
     default:
       return state;
   }
 }
+
+const messages = (state = [], action) => {
+  switch (action.type) {
+    case ADD_MESSAGE:
+      if (state.map(m => m.id).includes(action.id)) {
+        return state
+      } else {
+        return [
+          ...state,
+          message(undefined, action)
+        ];
+      }
+    case SEND_MESSAGE:
+      return [
+        ...state,
+        message(undefined, action)
+      ];
+    default:
+      return state;
+  }
+}
+
+export default messages
